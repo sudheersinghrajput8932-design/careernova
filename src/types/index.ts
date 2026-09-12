20f5aaef708f6db0b6825260c3a0b2932145104e
sudheersinghrajput8932-design <sudheersinghrajput8932@gmail.com>
2026-09-12 20:47:25 +0530

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,7 +10,11 @@ export type TabId =
   | 'about'
   | 'contact'
   | 'pricing'
-  | 'expertise'
+  | 'privacy'
+  | 'terms'
+  | 'disclaimer'
+  | 'refund'
+  | 'cookies'
   | '404';
 
 export type SubToolId =
